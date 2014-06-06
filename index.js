be6b4cc4593003da3b3744983037bf9fdc8fbc25
Matt Styles <matt@veryfizzyjelly.com>
2014-06-06 09:31:30 +0100

    #add - put and get

--- index.js
+++ index.js
@@ -18,11 +18,42 @@ module.exports = function( loc, opts ) {
     });
 }
 
-
 /**
  * Level-promisify object
  */
 var LP = function( db ) {
-    console.log( 'new LP object', db );
     this._level = db;
+}
+
+LP.prototype = {
+
+    /**
+     * Promise wrapper to ensure context
+     */
+    _promise: function( fn ) {
+        return new Promise( fn.bind( this ) );
+    },
+
+    /**
+     * LevelUp API
+     */
+
+    put: function( key, val, opts ) {
+        return this._promise( function( resolve, reject ) {
+            this._level.put( key, val, opts, function( err ) {
+                if ( err ) reject ( err );
+                resolve();
+            });
+        });
+    },
+
+
+    get: function( key, opts ) {
+        return this._promise( function( resolve, reject ) {
+            this._level.get( key, opts, function( err, val ) {
+                if ( err ) reject( err );
+                resolve( val );
+            });
+        });
+    }
 }
